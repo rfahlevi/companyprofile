019d920cdd71ac0dbd12e5343e24fcef078acf71
rfahlevi <rezafahlevi55@gmail.com>
2020-05-08 22:01:36 +0700

    Add whatsapp button

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 const sideNav = document.querySelectorAll('.sidenav');
 M.Sidenav.init(sideNav);
 
+// Slider
 const slider = document.querySelectorAll('.slider');
 M.Slider.init(slider, {
     indicators: false,
@@ -10,14 +11,26 @@ M.Slider.init(slider, {
     interval: 3000,
 });
 
+// Parallax
 const parallax = document.querySelectorAll('.parallax');
 M.Parallax.init(parallax);
 
+// Material Box
 const materialbox = document.querySelectorAll('.materialboxed');
 M.Materialbox.init(materialbox);
 
+// Scrollspry
 const scrollspy = document.querySelectorAll('.scrollspy');
 M.ScrollSpy.init(scrollspy, {
     scrollOffset: 50,
-    throttle: 10
+    throttle: 200,
+});
+
+// Jquery
+$(window).on('load', function() {
+    $('.animate').each(function(i) {
+        setTimeout(function() {
+            $('.animate').eq(i).addClass('show');
+        }, 300 * i)
+    })
 });
